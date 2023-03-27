3b6be752d7719bfe39bf7bc72dd4f62a4711a1c0
SaicuCarina <carina.saicu@yahoo.com>
2023-03-27 17:27:12 +0300

    intermediar

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require('fs');
+const path=require('path');
 
 ob_global={
     ob_erori:null,
@@ -8,10 +9,29 @@ ob_global={
 
 app= express();
 console.log("Folder proiect", __dirname);
+console,log("Cale fisier", __filename);
+console.log("Director de lucrare", process.cwd());
+
+vectorFoldere=["temp", "temp1"]
+for (let folder of vectorFoldere){
+    // let caleFolder=__dirname+"/"+folder;
+    let caleFolder=path.join(__dirname, folder);
+    if(!fs.existsSync(caleFolder))
+        fs.mkdirSync(caleFolder);
+}
 
 app.set("view engine", "ejs");
 
-app.use("/resurse", express.static(__dirname+"/resurse"));
+app.use("/resurse", express.static(__dirname+"/resurse"));  
+
+app.use(/^\/resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, function(req,res){
+    afisare_erori(res, 403);
+
+})
+
+app.get("/favicon.ico", function(req, res){
+    res.sendFile(__dirname+"/resurse/ico/favicon.ico"); //aici nu stiu daca e bine
+})
 
 app.get("/ceva", function(req, res){
     res.send("altceva");
@@ -21,11 +41,19 @@ app.get(["/index","/", "/home"], function(req, res){
     res.render("pagini/index", {ip: req.ip, a: 10, b: 20});
 })
 
+//app.get(/ [a-zA-Z0-9]\.ejs$/ )
+
+app.get("/*.ejs", function(req, res){
+    afisare_erori(res, 400);
+})
+
 app.get("/*", function(req, res){
+    try{
     res.render("pagini"+req.url, function(err, rezRandare){
         if(err){
             if(err.message.startsWith("Failed to lookup view"))
-                afisare_erori(res, 404);
+                afisare_erori(res, _identificator=404, "ceva");
+                // afisareEroare(res, _identificator: 404, _titlu: "ceva"{});
             else
                 afisare_erori(res);
             console.log(err);           
@@ -36,6 +64,11 @@ app.get("/*", function(req, res){
             res.send(rezRandare);
         }
     });
+    }catch(err){
+        if(err.message.startsWith("Cannot find module")){
+            afisare_erori(res, 404);
+        }
+    }
 })
 
 function initializeaza_erori(){
@@ -51,11 +84,16 @@ function initializeaza_erori(){
 }
 initializeaza_erori();
 
-function afisare_erori(res, _identificator, _titlu, _text, _imagine){
+//function afisare_erori(res, _identificator, _titlu=, _text, _imagine = {}) //trimite ca obiect ( destructing )
+//name parameters mai sus, si mai jos parametrii default
+function afisare_erori(res, _identificator, _titlu="titlu default", _text, _imagine){
     let v_erori = ob_global.ob_erori.info_erori;
     let eroare = v_erori.find(function(elem){return elem.identificator == _identificator;})
     if(eroare){
-        let titlu1 = _titlu || eroare.titlu;
+        let titlu1 = _titlu == "titlu default" ? (eroare.titlu || titlu) : titlu;
+        //daca programaorul selecteaza titlul, se ia titlul din argument,
+        //daca nu e setat, se ia cel din json,
+        //daca nu avem titlu nici in json, se ia titlul din valoarea default
         let text1 = _text || eroare.text;
         let imagine1 = _imagine || eroare.imagine;
         if(eroare.status)
@@ -65,7 +103,7 @@ function afisare_erori(res, _identificator, _titlu, _text, _imagine){
     }
     else{
         let eroare_dif=ob_global.ob_erori.eroare_dif;
-        res.render("pagini/eroare", {titlu:eroare_dif.titlu, text:eroare_dif.text, imagine:eroare_dif.imagine});
+        res.render("pagini/eroare", {titlu:eroare_dif.titlu, text:eroare_dif.text, imagine:ob_global.ob_erori1.cale_baza = "/" + err.imagine});
     }
 }
 // afisare_erori();
