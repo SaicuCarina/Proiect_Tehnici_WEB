bc39b79578646a222f03d350ac41de5d70ba085e
SaicuCarina <93483071+SaicuCarina@users.noreply.github.com>
2023-04-04 11:04:11 +0300

    Etapa4

--- index.js
+++ index.js
@@ -115,6 +115,7 @@ const express = require("express");
 // require e ca un import din C (express e o biblioteca, dar e un fel de obiect)
 const fs = require("fs"); // fs e biblioteca de file system
 const path = require("path");
+const sharp = require("sharp");
 
 obGlobal = {
     obErori: null,
@@ -131,23 +132,23 @@ console.log("Director de lucru: ", process.cwd());
 app.set("view engine", "ejs"); // motor de template 
 // app.set trebuie pus inainte de get-uri 
 
-app.use("/Resurse", express.static(__dirname + "/Resurse"));
+app.use("/resurse", express.static(__dirname + "/resurse"));
 // express.static e o functie care returneaza un obiect
 // asa "livrez" resursele pentru site 
 
-app.use(/^\/Resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, function (req, res) {
+app.use(/^\/resurse(\/[a-zA-Z0-9]*(?!\.)[a-zA-Z0-9]*)*$/, function (req, res) {
     afiseazaEroare(res, 403);
 })
 
 app.get("/favicon.ico", function (req, res) {
-    res.sendFile(__dirname + "/Resurse/Imagini/favicon.ico");
+    res.sendFile(__dirname + "/resurse/imagini/carte2.jpg");
 });
 
 // ejs e pentru a include cod html in alte fisiere html
 
 
 app.get(["/index", "/", "/home"], function (req, res) {
-    res.render("pagini/index", { ip: req.ip, a: 10, b: 20 });
+    res.render("pagini/index", { ip: req.ip, a: 10, b: 20, imagini: obGlobal.obImagini.imagini});
 }); //render - compileaza ejs-ul si il trimite catre client
 // render stie ca e folosit pentru template, si se uita in views (folderul default)
 app.get("/despre", function (req, res) {
@@ -197,7 +198,7 @@ app.get("/*", function (req, res) {
 
 
 function initErori() {
-    var continut = fs.readFileSync(__dirname + "/Resurse/json/erori.json").toString("utf-8"); // asteptam raspuns ( se pun intr-o coada de taskuri)
+    var continut = fs.readFileSync(__dirname + "/resurse/json/erori.json").toString("utf-8"); // asteptam raspuns ( se pun intr-o coada de taskuri)
     //pentru functie asyncrona nu se asteapta raspuns 
     // console.log(continut);
     obGlobal.obErori = JSON.parse(continut);
@@ -215,6 +216,38 @@ function initErori() {
 }
 
 initErori();
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+function initImagini() {
+    var continut = fs.readFileSync(__dirname + "/resurse/json/galerie.json").toString("utf-8"); // asteptam raspuns ( se pun intr-o coada de taskuri)
+    //pentru functie asyncrona nu se asteapta raspuns 
+    obGlobal.obImagini = JSON.parse(continut);
+    let vImagini = obGlobal.obImagini.imagini;
+
+    let caleAbs=path.join(__dirname,obGlobal.obImagini.cale_galerie);
+    let caleAbsMediu=path.join(caleAbs, "mediu");
+    if(!fs.existsSync(caleAbsMediu))
+        fs.mkdirSync(caleAbsMediu);
+
+
+     for (let imag of vImagini) {
+        [numeFis, ext] = imag.fisier.split(".");
+        imag.fisier_mediu="/"+path.join(obGlobal.obImagini.cale_galerie, "mediu", numeFis+".webp");
+        let caleAbsFisMediu=path.join(__dirname, imag.fisier_mediu);
+        sharp(path.join(caleAbs, imag.fisier)).resize(400).toFile(caleAbsFisMediu);
+
+        // console.log(vErori[i].imagine);
+
+        imag.fisier="/"+path.join(obGlobal.obImagini.cale_galerie, imag.fisier);
+
+     } 
+    // for (let eroare of vErori) { //echivalent cu iteratorul din C++
+    //     eroare.imagine = "/" + obGlobal.obErori.cale_baza + "/" + eroare.imagine;
+    //     // eroare.imagine = path.join(obGlobal.obErori.cale_baza, eroare.imagine);
+    // }
+}
+
+initImagini();
 
 //function afiseazaEroare(res, _identificator, _titlu =, _text, _imagine = {}) //trimitere ca obiect ( destructuring ) 
 //name parameters mai sus, si mai jos parametrii default 
